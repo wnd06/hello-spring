ad53bf88874c1399f6802b67d53a24b79d77f5ec
wnd06 <min990605@naver.com>
2023-04-08 00:20:09 +0900

    mvc, 템플릿 엔진

--- src/main/java/hello/hellospring/controller/hellocontroller.java
+++ src/main/java/hello/hellospring/controller/hellocontroller.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class hellocontroller {
@@ -11,5 +12,10 @@ public class hellocontroller {
     public String hello(Model model) {
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+    @GetMapping("Hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
